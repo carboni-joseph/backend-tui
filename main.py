6f1958769edf6aa1a53ef38b243554faa3f43eae
carboni-joseph <joe@carbonitech.com>
2024-09-30 17:15:11 -0400

    cleaned up

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 """Checks for version endpoint response from the API to determine which module to run"""
 
-import os
+from os.path import dirname, abspath
 import requests as r
 from pathlib import Path
 from configparser import ConfigParser
 
+FILE_DIR = Path(dirname(abspath(__file__)))
 CONFIGS = ConfigParser()
-CONFIGS.read(str(Path(os.path.dirname(os.path.abspath(__file__))) / "config.ini"))
+CONFIGS.read(str(FILE_DIR / "config.ini"))
 BACKEND_URL = CONFIGS["ENDPOINTS"]["backend_url"]
 V2_AVAILABILITY_ENDPOINT = BACKEND_URL + "/v2"
 
-resp = r.get(str(V2_AVAILABILITY_ENDPOINT))
+resp = r.get(V2_AVAILABILITY_ENDPOINT)
 match resp.status_code:
     case 200:
         import main_v2
